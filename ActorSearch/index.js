85e76f7ccec8768ea9d7f446f239ae3d65360a14
HH <hhubik@vis.org>
2018-08-18 11:26:40 -0400

    Implemented ActorSearch

---
+++ ActorSearch/index.js
@@ -0,0 +1,40 @@
+module.exports = function (context, req) {
+    var fs = require('fs');
+
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    if (req.query.q || (req.body && req.body.q)) {
+
+        var filepath = './PersonSearchJS/sample.dat';
+        var queryString = (req.query.q) ? req.query.q : req.body.q;
+
+        switch (queryString) {
+            case 'empty':
+                filepath = './ActorSearch/empty-data.json'
+                break;
+            case 'small':
+                filepath = './ActorSearch/small-data.json'
+                break;
+            case 'max':
+                filepath = './ActorSearch/max-data.json'
+                break;
+        }
+
+        var data = fs.readFileSync(filepath, 'utf-8');
+
+        context.res = {
+            // status: 200, /* Defaults to 200 */
+            body: data
+        };
+    }
+    else {
+        var error = fs.readFileSync('error.json', 'utf-8');
+
+        context.res = {
+            status: 400,
+            // body: "Please pass a name on the query string or in the request body"
+            body: error
+        };
+    }
+    context.done();
+};
--- README.md
+++ README.md
@@ -14,6 +14,24 @@ This is a test web server that emulates Microsoft Azure Functions used by [PolTr
 
 2. `func host start` starts the HTTP server and prints the list of _available_ Azure functions in the shell window.
 
+### Debugging Azure Functions
+
+1. Attach to the `node` process started by Step #2 above.
+2. Add a breakpoint in `index.js` of an Azure function.
+3. Access the Azure function URL. For example `http://localhost:7071/api/ActorSearch?q=small`
+
 ## API
 
-TBA
+Poltrack Test Server implements an API very loosely based on [JSON API Specification](http://jsonapi.org/).
+
+### Actor Search Function
+
+`http://localhost:7071/api/ActorSearch?q=<query_string>`
+
+where `query_string` is `empty | small | max`
+
+Returns an array of `actor search result` objects.  (Political) `actor` is usually a person who is now, or was in the past, holding a political office.  Actors can take actions of certain type.
+
+### Provider Search Function
+
+Returns an array of `provider search result` objects.  `provider` is an entity (organization or person) that provided information about an action taken by an `actor`.
